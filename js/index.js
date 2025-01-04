c4d23f52a3c7b97049ba7741d663c2c2d15cebb8
poker2121 <banoobnagy@gmail.com>
2025-01-05 00:16:45 +0200

    more details

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next', 
+    prevEl: '.swiper-button-prev', 
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
